3647ca040f542a1136893f73a764e055a9c6ab92
DevanshBhatt <devanshbhatt10@gmail.com>
2023-09-12 14:30:15 +0530

    portchange

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const appRouter = require('./app');
 const app = express()
-const port= process.env.PORT || 3000;
+const port= process.env.PORT || 8080;
 
 app.use(express.json())
 app.use(appRouter)
